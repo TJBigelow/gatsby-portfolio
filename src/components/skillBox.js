85211a6d075db68bfbdef341e731c039a6813317
tjbigelow <tjbigelow@att.net>
2021-03-07 18:05:35 -0600

    added temp skills before building cms

--- src/components/skillBox.js
+++ src/components/skillBox.js
@@ -1,14 +1,20 @@
 import React from "react"
 
-const skillBox = () => {
+const renderSkills = skills => {
+  return skills.map(skill => {
+    return (
+      <li className="skill-box__item">
+        <span className="skill-box__name">{skill}</span>
+      </li>
+    )
+  })
+}
+
+const skillBox = props => {
   return (
     <div className="skill-box">
-      <h3 className="skill-box__heading">Heading</h3>
-      <ul className="skill-box__content">
-        <li className="skill-box__item">
-          <span className="skill-box__name">Skill</span>
-        </li>
-      </ul>
+      <h3 className="skill-box__heading">{props.heading}</h3>
+      <ul className="skill-box__content">{renderSkills(props.skills)}</ul>
     </div>
   )
 }
